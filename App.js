f6507648bb11a7a9e95a4bc5f54fc8d1dd058cc2
Suman-N <84329474+Suman-N@users.noreply.github.com>
2021-08-02 13:25:55 +0530

    Adding Parent Component

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tempreture from "../src/Component/Tempreture";
+
+const App = () => {
+  return (
+    <>
+      <Tempreture/>
+    </>
+  )
+}
+
+export default App;
+
